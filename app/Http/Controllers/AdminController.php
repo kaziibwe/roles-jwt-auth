1afaab294428a74f1647181fa9a63b5a5100d2b5
alfredkaziibwe19@gmail.com <alfredkaziibwe19@gmail.com>
2024-07-09 10:09:05 -0400

    update doctor

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use auth;
 use App\Models\Admin;
+use App\Models\Doctor;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\Hash;
@@ -12,55 +13,101 @@ use Illuminate\Validation\ValidationException;
 class AdminController extends Controller
 {
     //
-    public function _construct() {
-        $this->middleware('auth:admin-api',['except'=>['adminlogin','adminregister']]);
-   }
-    public function Adminregister(Request $request){
+    public function _construct()
+    {
+        $this->middleware('auth:admin-api', ['except' => ['adminlogin', 'adminregister']]);
+    }
+    public function Adminregister(Request $request)
+    {
 
-        try{
-        $data = $request->validate([
-            "name"=>"required | string",
-            'email' => ['required', 'email', Rule::unique('admins', 'email')],
-            "phone"=>"required | string",
-            'password'=>'required',
+        try {
+            $data = $request->validate([
+                "name" => "required | string",
+                'email' => ['required', 'email', Rule::unique('admins', 'email')],
+                "phone" => "required | string",
+                'password' => 'required',
 
-        ]);
+            ]);
 
-        if ($request->hasFile('image')) {
-            $data['image'] = $request->file('image')->store('images', 'public');
+            if ($request->hasFile('image')) {
+                $data['image'] = $request->file('image')->store('images', 'public');
+            }
+            $data['password'] = bcrypt($data['password']);
+            $admin = Admin::create($data);
+
+            if ($admin) {
+                return response()->json(["admin" => $admin, 'status' => true], 200);
+            } else {
+                return response()->json(['status' => false], 200);
+            }
+        } catch (ValidationException $e) {
+            // Return JSON response with validation errors
+            return response()->json([
+                'errors' => $e->errors(), // Detailed validation errors
+            ], 422);
+        } catch (\Exception $e) {
+            // Catch any other exceptions and return a generic error response
+            return response()->json([
+                'error' => $e->getMessage(), // Detailed error message
+            ], 500);
         }
-        $data['password'] = bcrypt($data['password']);
-        $admin=Admin::create($data);
-
-        if($admin){
-            return response()->json(["admin"=>$admin,'status'=>true],200);
-        }else{
-            return response()->json(['status'=>false],200);
-        }
-
-
-} catch (ValidationException $e) {
-    // Return JSON response with validation errors
-    return response()->json([
-        'errors' => $e->errors(), // Detailed validation errors
-    ], 422);
-} catch (\Exception $e) {
-    // Catch any other exceptions and return a generic error response
-    return response()->json([
-        'error' => $e->getMessage(), // Detailed error message
-    ], 500);
-}
-
     }
 
-    public function adminlogin(Request $request){
-            $credentials = request(['email', 'password']);
 
-            if (! $token = auth()->guard('admin-api')->attempt($credentials)) {
-                return response()->json(['error' => 'Unauthorized'], 401);
+
+    public function adminUpdate(Request $request, $id)
+    {
+
+        try {
+            $user = Admin::find($id);
+            // return  $request->all();
+
+
+            if (!$user) {
+                return response()->json(['status' => false], 200);
             }
-            return $token;
+            $data = $request->validate([
+                "name" => "nullable ",
+                'email' => "nullable|email",
+                "phone" => "nullable ",
+                'password' => 'nullable',
 
+            ]);
+
+            if ($request->hasFile('image')) {
+                $data['image'] = $request->file('image')->store('images', 'public');
+            }
+            if (!empty($data['password'])) {
+                $data['password'] = Hash::make($data['password']);
+
+            } else {
+                unset($data['password']);
+            }
+
+            Admin::where('id', $id)->update($data);
+
+            return response()->json(["message" => "Admin updated successfully", 'status' => true], 200);
+        } catch (ValidationException $e) {
+            // Return JSON response with validation errors
+            return response()->json([
+                'errors' => $e->errors(), // Detailed validation errors
+            ], 422);
+        } catch (\Exception $e) {
+            // Catch any other exceptions and return a generic error responsee
+
+            return $e->getMessage();
+            return response()->json(["message" => "Server error"], 200);
+        }
+    }
+
+    public function adminlogin(Request $request)
+    {
+        $credentials = request(['email', 'password']);
+
+        if (!$token = auth()->guard('admin-api')->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+        return $token;
     }
 
     // protected function respondWithToken($token)
@@ -91,16 +138,14 @@ class AdminController extends Controller
 
 
 
-public function getSingleAdmin($id)
-{
-    $Admin = Admin::find($id);
-    if (!$Admin) {
-        return response()->json(['message' => 'Admin Not Found']);
+    public function getSingleAdmin($id)
+    {
+        $Admin = Admin::find($id);
+        if (!$Admin) {
+            return response()->json(['message' => 'Admin Not Found']);
+        }
+        return response()->json([
+            'Admin' => $Admin
+        ]);
     }
-    return response()->json([
-        'Admin' => $Admin
-    ]);
-}
-
-
 }
